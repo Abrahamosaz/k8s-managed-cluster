5196d67aedcd30bb8842c8dc8cc15d9520f1b6e4
Abraham Omorisiagbon <abrahamosazee3@gmail.com>
2026-04-04 12:11:55 +0100

    add ips as out variables

--- control_plane.sh
+++ control_plane.sh
@@ -8,13 +8,13 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 
-# Install CNI (Networking) – Calico
-kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
-
+# Install CNI (Networking) – Calico v3.31.x (Tigera tests this line against Kubernetes through 1.35)
+# https://docs.tigera.io/calico/latest/getting-started/kubernetes/requirements
+CALICO_VERSION=v3.31.4
+kubectl apply -f "https://raw.githubusercontent.com/projectcalico/calico/${CALICO_VERSION}/manifests/calico.yaml"
 
 # Verify master node
 kubectl get nodes
-
 
 # Get join command (IMPORTANT)
 kubeadm token create --print-join-command
@@ -22,7 +22,6 @@ kubeadm token create --print-join-command
 # Example output:
 kubeadm join 192.168.1.10:6443 --token abc123.xyz \
 --discovery-token-ca-cert-hash sha256:xxxxx
-
 
 
 # Reset cluster (if needed)
--- terraform/output.tf
+++ terraform/output.tf
@@ -9,3 +9,16 @@ output "public_subnet_ids" {
 output "private_subnet_ids" {
   value = aws_subnet.private_subnet[*].id
 }
+
+output "ec2_instances_ips" {
+  value = {
+    master = {
+      private_ip = aws_instance.master_node.private_ip
+      public_ip  = aws_instance.master_node.public_ip
+    }
+    workers = [for w in aws_instance.worker_node : {
+      private_ip = w.private_ip
+      public_ip  = w.public_ip
+    }]
+  }
+}
